fc830e1f5f776f756d0b1a53d0cb7066d277368b
LotiD <didierloti1@gmail.com>
2025-09-20 19:15:56 +0200

    add categories

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import productRoutes from './products';
+import categoryRoutes from './categories';
 
 export default async function apiRoutes(fastify: FastifyInstance) {
   await fastify.register(productRoutes, { prefix: '/products' });
+  await fastify.register(categoryRoutes, { prefix: '/categories' });
 }
