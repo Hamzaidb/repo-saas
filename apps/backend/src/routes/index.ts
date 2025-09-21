1190afaf65a5bea6832bf43a3f66aca3815112bf
LotiD <didierloti1@gmail.com>
2025-09-21 10:12:46 +0200

    fix routes users

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import productRoutes from './products';
 import categoryRoutes from './categories';
+import userRoutes from './users';
 
 export default async function apiRoutes(fastify: FastifyInstance) {
   await fastify.register(productRoutes, { prefix: '/products' });
   await fastify.register(categoryRoutes, { prefix: '/categories' });
+  await fastify.register(userRoutes, { prefix: '/users' });
 }
